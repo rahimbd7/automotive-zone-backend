ca1412f45c92e119f631adaaf59f956ae857f08f
rahimbd7 <rahimbd7@gmail.com>
2024-02-19 17:29:48 +0600

    initialized the project, connected with mongodDB Atlas, defined neccessary api's for communicated with frontend to backend as well database

---
+++ index.js
@@ -0,0 +1,107 @@
+//oR4aMimZawl6da8x
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+require('dotenv').config()
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xamqemm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const usersCollection = client.db('automotiveDB').collection('users');
+        const productsCollection = client.db('automotiveDB').collection('products');
+        const cartsCollection = client.db('automotiveDB').collection('carts');
+
+        // Users Related APIs
+        app.post('/users/add', async (req, res) => {
+            const data = req.body;
+        })
+
+      
+        // Brand Related APIs
+        // Products Related APIs
+        app.get('/products', async (req, res) => {
+            
+        })
+
+        app.get('/products/details/:id', async (req, res) => {
+            const id = req.params.id;
+        })
+
+        app.post('/products/add', async (req, res) => {
+            const data = req.body;
+        })
+
+        app.delete('/products/delete/:id', async (req, res) => {
+            const id = req.params.id;
+        })
+
+        app.put('/products/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+        })
+
+        // Carts Related APIs
+
+        app.get('/carts', async (req, res) => {
+            res.send('all the carts')
+        })
+
+        app.post('/carts/add', async (req, res) => {
+            const data = req.body;
+            const result = await cartsCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.delete('/carts/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+        })
+
+        
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Running backend by express...')
+})
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`)
+})
